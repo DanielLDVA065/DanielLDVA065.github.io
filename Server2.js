e30c4e85bfb7c76f109eaca7adefa67523be4e47
DanielLDVA065 <A01641413@tec.mx>
2026-04-21 10:11:22 -0600

    Add basic HTTP server handling query param x
    Add Server2.js: a simple Node.js HTTP server that parses the URL query parameter `x` and returns plain-text responses for `x == 1` ("X es 1") and `x == 2` ("X es 2"). The server logs the query param and listens on port 1984. File includes some commented example code and logging for debugging.

---
+++ Server2.js
@@ -0,0 +1,30 @@
+import http from 'http';
+import url from 'url';
+
+const servidor = http.createServer((req, res) => {
+  
+  //console.log(req); 
+  const urlProcesada = url.parse(req.url, true);
+  //console.log(urlProcesada);
+  const queryParams = urlProcesada.query;
+  console.log(queryParams.x);
+
+  if (queryParams.x == 1){    
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('X es 1');
+  }
+  if (queryParams.x == 2){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('X es 2');
+
+  }
+  //console.log("Alguien me mando una solicitud");
+  //res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //res.end('Quiero la libertad de esculpir y cincelar mi propio rostro, de detener la hemorragia con cenizas, de crear mis propios dioses a partir de mis entrañas...\n');
+});
+
+const puerto = 1984;
+
+servidor.listen(puerto, () => {
+  console.log(`Servidor escuchando en el puerto ${puerto}`);
+});
